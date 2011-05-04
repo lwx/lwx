d814db47c392626c87242f483af81266e55e0304
Lim Wei Xuan <enigmawx@gamil.com>
2011-05-04 16:00:43 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lwx::Application.config.secret_token = '74e84f0c81fa55c260607c3d632602438b1cc0d691463761535015b9ae9c638a90e5b930cec9bfedfb0a76378dc1d7a71d294c815eb9fd03a02858ab0d75d156'
